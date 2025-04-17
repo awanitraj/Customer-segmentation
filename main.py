e7c80b55802314410e1a40397ddb16b01f2d659f
Awanit  Raj <awanitraj@Awanits-MacBook-Air.local>
2025-04-17 12:03:03 +0530

    added functions

--- main.py
+++ main.py
@@ -11,6 +11,39 @@ df = load_data('data/mall_customers.csv')
 print("🔹 First 5 rows of the dataset:")
 print(df.head())
 
+print("\n🔍 DataFrame Info:")
+print(df.info())
+
+print("\n🧩 Missing Values Count:")
+print(df.isnull().sum())
+
+df_cleaned = df.dropna()
+
+
+def remove_outliers_iqr(df, column):
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+    return df[(df[column] >= lower) & (df[column] <= upper)]
+
+df_cleaned = remove_outliers_iqr(df, "Annual Income (k$)")
+df_cleaned = remove_outliers_iqr(df_cleaned, "Spending Score (1-100)")
+
+print("\n✅ Cleaned Data Info:")
+print(df_cleaned.info())
+
+print("\n📈 Cleaned Data Summary:")
+print(df_cleaned.describe())
+
+
+
+df["Annual Income (k$)"] = df["Annual Income (k$)"].fillna(df["Annual Income (k$)"].mean())
+df["Spending Score (1-100)"] = df["Spending Score (1-100)"].fillna(df["Spending Score (1-100)"].median())
+
+
+
 print("\n🔹 Summary statistics:")
 print(df.describe())
 
